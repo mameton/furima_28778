ec4d3d354cd084a397392e5aa62ad68eb1c6de02
mameton <out.of.escape@outlook.jp>
2020-09-25 18:24:24 +0900

    外部キー制約3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :items , dependent: :destroy
-  has_many :pays
+  has_many :pays , dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
